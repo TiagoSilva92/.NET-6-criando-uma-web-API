eb6cad9b64bf11e4b42b1eb611c3612f2b0d26bb
Tiago Silva <tiagodasilva360@hotmail.com>
2023-08-22 19:14:11 -0300

    Validando parâmetros recebidos

--- FilmesApi/FilmesApi/Models/Filme.cs
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -1,9 +1,16 @@
-﻿namespace FilmesApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
 {
     public class Filme
-    { 
-        public string Titulo { get; set; }    
+    {
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O genero do filme é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O tamanho do genero não pode exceder 50 caracteres")]
         public string Genero { get; set; }
+        [Required(ErrorMessage = "A duração do filme é obrigatória")]
+        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
