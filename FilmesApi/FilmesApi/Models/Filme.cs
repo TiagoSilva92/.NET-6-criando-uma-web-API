8640f45d2242c509484ce295e3895cf6674a7ab2
Tiago Silva <tiagodasilva360@hotmail.com>
2023-08-22 19:16:57 -0300

    adicionando restrições

--- FilmesApi/FilmesApi/Models/Filme.cs
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -5,12 +5,12 @@ namespace FilmesApi.Models
     public class Filme
     {
         [Required(ErrorMessage = "O título do filme é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O título do filme não pode exceder 50 caracteres")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "O genero do filme é obrigatório")]
-        [MaxLength(50, ErrorMessage = "O tamanho do genero não pode exceder 50 caracteres")]
-        public string Genero { get; set; }
-        [Required(ErrorMessage = "A duração do filme é obrigatória")]
-        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+        [Required(ErrorMessage = "O campo de duração é obrigatório")]
+        [Range(1, 360, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 360")]
         public int Duracao { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
     }
 }
