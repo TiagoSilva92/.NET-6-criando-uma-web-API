37e17cc66b1b4c4609000d710273e7b714bd0f17
Tiago Silva <tiagodasilva360@hotmail.com>
2023-08-24 18:52:59 -0300

    Consultando e paginando
    Consultando e paginando

--- FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -8,13 +8,32 @@ namespace FilmesApi.Controllers
     public class FilmeController : Controller
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
 
         [HttpPost]
-        public void AdcionaFilme([FromBody] Filme filme)
+        public IActionResult AdcionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
+            return CreatedAtAction(nameof(RecuperaFilmePorId), 
+                new {id = filme.Id}, 
+                filme);
+        }
+
+        [HttpGet()]
+        public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+        {
+            return filmes.Skip(skip).Take(10);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null)
+                return NotFound();
+            return Ok(filme);
         }
     }
 }
